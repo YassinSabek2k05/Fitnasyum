b3feea1c32c52730f07dbfc0cdc20fde6d6d415a
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-03-04 22:20:25 +0200

    add discover page with search functionality and recent searches display

--- src/app/discover/discover.page.ts
+++ src/app/discover/discover.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInput, IonSearchbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-Discover',
@@ -9,5 +10,33 @@ import { Component } from '@angular/core';
 export class DiscoverPage {
 
   constructor() {}
+  public data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ];
+  public recentSearches = ['Abs', 'Build Muscle', 'Yoga', 'Mountain Climbers', 'Cross Fit', 'Squats', 'Lunges', 'Upper Body'];
+  public results = [...this.data.slice(0, 3)];
+  autocompleteItems = ["hello", "world", "foo", "bar"];
+  handleInput(event: Event) {
+    const target = event.target as HTMLIonSearchbarElement;
+    const query = target.value?.toLowerCase() || '';
+    this.results = this.data.filter((d) => d.toLowerCase().includes(query));
+    this.results = this.results.slice(0, 3);
+  }
+  @ViewChild('searchBar') searchBar!: IonSearchbar;
 
+  ionViewDidEnter() {
+    this.searchBar.setFocus();
+  }
+  setPlaceHolder(str: string) {
+    this.searchBar.value = str;
+  }
 }
