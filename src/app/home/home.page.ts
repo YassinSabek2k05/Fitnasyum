00013f617f460042322da13ca7e5158b0d3c5c87
MustafaEhab <31803734+MustafaEhab@users.noreply.github.com>
2025-03-04 17:29:35 +0200

    home page finished
    home page finished

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,11 @@ export class HomePage {
     { title: '30 days Crunch', image: 'assets/30-challeng.png' },
     { title: '31 days Plank', image: 'assets/31days.png' },
     { title: '20 days squats', image: 'assets/20days.png' }];
-
+  blogs = [
+    { title: 'How to combat stress', image: 'assets/bruce-mars.png' },
+    { title: '5 Tips to get more active', image: 'assets/blog-2.png' },
+    { title: '5 simple exercises', image: 'assets/3rd-blog.png' }
+  ]
   constructor() { }
 
 }
