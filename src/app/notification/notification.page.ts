d10ec8916b39a2492bc2f641656612726fffe4ba
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-03-06 21:44:30 +0200

    rename tabs in discover and notification pages; implement notification display logic

--- src/app/notification/notification.page.ts
+++ src/app/notification/notification.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface Notification{
+  title: string;
+  seen: boolean;
+  date: Date;
+  content: string;
+  day: string;
+}
+
 @Component({
   selector: 'app-notification',
   templateUrl: 'notification.page.html',
@@ -7,7 +15,72 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class NotificationPage {
-
+  isSeen(notification: Notification): string {
+    if(notification.seen){
+      return 'seen';
+    }
+    return 'not-seen';
+  }
+  notifications: Notification[] = [
+    {
+      title: "Notification Title",
+      seen: false,
+      date: new Date(5, 6, 2025),
+      content: "ipsam, quod, quibusdam, quia voluptas, voluptatem, quos, doloremque",
+      day:  "Today",
+    },
+    {
+      title: "Notification Title",
+      seen: false,
+      date: new Date(5, 6, 2025),
+      content: "ipsam, quod, quibusdam, quia voluptas, voluptatem, quos, doloremque",
+      day:  "Today",
+    },
+    {
+      title: "Notification Title",
+      seen: false,
+      date: new Date(5, 6, 2025),
+      content: "ipsam, quod, quibusdam, quia voluptas, voluptatem, quos, doloremque",
+      day:  "Today",
+    },
+    {
+      title: "Notification Title",
+      seen: false,
+      date: new Date(5, 6, 2025),
+      content: "ipsam, quod, quibusdam, quia voluptas, voluptatem, quos, doloremque",
+      day:  "Today",
+    },
+    {
+      title: "Notification Title",
+      seen: false,
+      date: new Date(5, 6, 2025),
+      content: "ipsam, quod, quibusdam, quia voluptas, voluptatem, quos, doloremque",
+      day:  "Today",
+    },
+    {
+      title: "Notification Title",
+      seen: false,
+      date: new Date(5, 6, 2025),
+      content: "ipsam, quod, quibusdam, quia voluptas, voluptatem, quos, doloremque",
+      day:  "Today",
+    },
+    {
+      title: "Notification Title",
+      seen: true,
+      date: new Date(),
+      content: "Notification Content",
+      day: "Today",
+    }
+  ];
+  getDay(date: Date): string {
+    if(date.toDateString() === new Date().toDateString()){
+      return "Today";
+    }
+    if(date.toDateString() === new Date(new Date().setDate(new Date().getDate() - 1)).toDateString()){
+      return "Yesterday";
+    }
+    return date.toDateString();
+  }
   constructor() { }
 
 }
