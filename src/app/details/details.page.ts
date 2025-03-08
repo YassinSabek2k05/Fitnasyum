267f5b157aea491f5b9555972ed3020ebddefcab
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-03-08 15:00:22 +0200

    add video display and workout switch functionality to details page

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   standalone: false,
 })
 export class DetailsPage implements OnInit {
+  workout: boolean = false;
   id: string | null = null;
   data: any;
   constructor(private route: ActivatedRoute ) { }
@@ -19,21 +20,38 @@ export class DetailsPage implements OnInit {
   loadData(id: string | null) {
     if(!id) return;
 
-    const details: { [key: string]: { title: string; description: string } } = {
+    const details: { [key: string]: { title: string; description: string, image: string } } = {
       '1': {
         title: 'Title 1',
-        description: 'Description 1'
+        description: 'Description 1',
+        image: 'assets/imgs/edgar-chaparro-sHfo3.png'
       },
       "2": {
         title: 'Title 2',
-        description: 'Description 2'
+        description: 'Description 2',
+        image: 'assets/imgs/edgar-chaparro-sHfo3.png'
+
       },
       '3': {
         title: 'Title 3',
-        description: 'Description 3'
+        description: 'Description 3',
+        image: 'assets/imgs/edgar-chaparro-sHfo3.png'
+
       },
     }
     this.data = details[id] || { title: 'Not Found', description: 'Not Found' };
+  }
+  switchToWorkout(i: number) {
+    if(i === 1 && !this.workout) {
+      return 'show'; 
+    };
+    if(i === 2 && this.workout) {
+      return 'show';
+    }
+    return 'hide';
+  }
+  switchWorkout(){
+    this.workout = !this.workout;
   }
 
 }
