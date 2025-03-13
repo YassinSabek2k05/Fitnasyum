5be64754e241c2fcda3cbbbaf827b23fce610930
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-03-13 16:23:54 +0200

    add back button and improve details page layout; enhance styling and functionality

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   standalone: false,
 })
 export class DetailsPage implements OnInit {
-  workout: boolean = false;
+  workout: boolean = true;
   id: string | null = null;
   data: any;
   constructor(private route: ActivatedRoute ) { }
@@ -20,23 +20,25 @@ export class DetailsPage implements OnInit {
   loadData(id: string | null) {
     if(!id) return;
 
-    const details: { [key: string]: { title: string; description: string, image: string } } = {
+    const details: { [key: string]: { title: string; description: string, image: string, setsDetails: number[] } } = {
       '1': {
-        title: 'Title 1',
+        title: 'Advance Workout',
         description: 'Description 1',
-        image: 'assets/imgs/edgar-chaparro-sHfo3.png'
+        image: 'assets/imgs/edgar-chaparro-sHfo3.png',
+        setsDetails: [16,12,30]
       },
       "2": {
         title: 'Title 2',
         description: 'Description 2',
-        image: 'assets/imgs/edgar-chaparro-sHfo3.png'
+        image: 'assets/imgs/edgar-chaparro-sHfo3.png',
+        setsDetails: [16,12,30]
 
       },
       '3': {
         title: 'Title 3',
         description: 'Description 3',
-        image: 'assets/imgs/edgar-chaparro-sHfo3.png'
-
+        image: 'assets/imgs/edgar-chaparro-sHfo3.png',
+        setsDetails: [16,12,30]
       },
     }
     this.data = details[id] || { title: 'Not Found', description: 'Not Found' };
